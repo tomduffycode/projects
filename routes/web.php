9d913aa271a45520ecd4028e7a36ea61031c8930
Tom Duffy <thomas.edward.duffy@gmail.com>
2020-09-24 20:47:16 -0700

    created view for creating a project

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::group(['middleware' => 'auth:sanctum', 'verified'], function () {
     })->name('dashboard');
 
     Route::get('/projects', [ProjectsController::class, 'index']);
+    Route::get('/projects/create', [ProjectsController::class, 'create']);
     Route::get('/projects/{project}', [ProjectsController::class, 'show']);
     Route::post('/projects', [ProjectsController::class, 'store']);
 });
