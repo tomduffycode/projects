f756a8678fc3f292f0172d92efcbc53e476338a4
Tom Duffy <thomas.edward.duffy@gmail.com>
2020-10-02 12:27:21 -0700

    created task model, tests, migration

---
+++ database/migrations/2020_10_02_191714_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->date('due_date');
+            $table->unsignedBigInteger('assigned_to');
+            $table->unsignedBigInteger('project_id');
+            $table->timestamp('completed');
+            $table->timestamps();
+
+            $table->foreign('assigned_to')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
---
+++ tests/Unit/TaskTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Project;
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_belongs_to_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $project = Project::factory()->create();
+        
+        $task = Task::factory()->create(['project_id' => $project->id]);
+
+        
+    }
+}
